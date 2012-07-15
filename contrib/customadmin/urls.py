7b0b88e65cdec0d4f353dc177cd41ea6139e0c91
20tab <info@20tab.com>
2012-07-15 13:26:56 +0300

    Update master

--- contrib/customadmin/urls.py
+++ contrib/customadmin/urls.py
@@ -1,3 +1,6 @@
+"""
+It defines the urlpattern for css file with rules for admin's interface customization.
+"""
 from django.conf.urls.defaults import *
 urlpatterns = patterns('',
         (r'^upy_custom_admin_layout.css$','upy.contrib.customadmin.views.custom_admin_layout'),
