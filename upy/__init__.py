e0da5acb5ce95e856d64817fb3d08428c4791315
Raffaele Colace <raffaele@upcode.it>
2012-12-06 17:50:39 +0100

    issue 33 fixed

--- upy/__init__.py
+++ upy/__init__.py
@@ -1,3 +1,3 @@
-VERSION = (0,13,05)
+VERSION = (0,13,06)
 __version__ = '.'.join(map(str, VERSION))
-DATE = "2012-12-05"
+DATE = "2012-12-06"
--- upy/contrib/g11n/__init__.py
+++ upy/contrib/g11n/__init__.py
@@ -1,8 +1,11 @@
+ONUWSGI = False
+try:
+    import uwsgi
+    ONUWSGI = True
+except: 
+    pass
 from django.conf import settings
-if 'upy.contrib.g11n' in settings.INSTALLED_APPS:
-    try:
-        from upy.contrib.g11n.management.commands import g11n_validate
-        c = g11n_validate.Command()
-        c.execute()
-    except Exception, e:
-        print "\nError in upy.contrib.g11n.__init__: ",e
+if 'upy.contrib.g11n' in settings.INSTALLED_APPS and not ONUWSGI:
+    from upy.contrib.g11n.management.commands import g11n_validate
+    c = g11n_validate.Command()
+    c.execute()
