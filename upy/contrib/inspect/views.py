d00fdb86ae0863c62bdbd4fb39053c94c452447a
Raffaele Colace <raffaele@upcode.it>
2013-06-18 23:52:54 +0200

    django-inspect-model added as contrib inspect

--- upy/contrib/inspect/views.py
+++ upy/contrib/inspect/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import Http404
 from django.db import models
 from upy.contrib.inspect.utils import InspectModel
+from upy.contrib.g11n.models import G11nBase
 
 
 @login_required
@@ -19,14 +20,24 @@ def browse(request):
         apps.append(item) 
     
     model_res = None
+    g11n_model = None
+    display_as = None
     if request.POST:
         model_name=request.POST['model'].replace('.models','')
+        app = '.'.join(request.POST['model'].split('.')[:-1])
         model = models.get_model(model_name.split('.')[-2],model_name.split('.')[-1])
         model_res = InspectModel(model)
+        display_as = request.POST['display_as']
+        
+        if issubclass(model,G11nBase):
+            g11n_model = model.G11nMeta.g11n
+        
     if request.user.is_superuser:
         return render_to_response('upy_inspect_browse.html',
                                   {'apps':apps,
                                    'model':model_res,
-                                   'display_as':request.POST['display_as']},
+                                   'g11n_model':g11n_model,
+                                   'display_as':display_as,
+                                   'app':app},
                           context_instance=RequestContext(request))
     raise Http404
