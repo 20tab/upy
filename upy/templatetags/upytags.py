11cbaa9ef9773f0214e2c4f0ee93305a3c60db72
Raffaele Colace <raffaele@upcode.it>
2013-06-27 17:39:11 +0200

    uRange

--- upy/templatetags/upytags.py
+++ upy/templatetags/upytags.py
@@ -24,3 +24,24 @@ def comma2dot(value):
     if not val:
         return value
     return val[0].replace(',', '.')
+
+@register.filter
+def uRange(value):
+    """
+    Filter - returns a list containing range made from given value
+    Usage (in template):
+
+    <ul>{% for i in 3|uRange %}
+      <li>{{ i }}. Do something</li>
+    {% endfor %}</ul>
+
+    Results with the HTML:
+    <ul>
+      <li>0. Do something</li>
+      <li>1. Do something</li>
+      <li>2. Do something</li>
+    </ul>
+
+    Instead of 3 one may use the variable set in the views
+    """
+    return xrange( value )
