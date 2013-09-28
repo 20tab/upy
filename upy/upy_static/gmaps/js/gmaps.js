b660d78dad3a54f655342ebf345e4574f63afe54
Filippo Morelli <fi.morelli@gmail.com>
2013-09-28 19:15:02 +0200

    Extension of the previous behaviour (updating country select box according to selected continent) to Django Admin inlines too and reverse behaviour added (from countries list to continent)

--- upy/upy_static/gmaps/js/gmaps.js
+++ upy/upy_static/gmaps/js/gmaps.js
@@ -1,11 +1,29 @@
 jQuery(function(){
 	
-	var country = $('#id_country').html();
-		
-	$('#id_continent').on('change',function(){
-		var val = $(this).val();
-		$('#id_country').html(country);
-		$('#id_country').children().not('.'+val).remove("optgroup");
+	//append the 'on' event to all the country select boxes (live)
+	$('body').on('change', '[id^="id_"][id$="country"]', function(){
+		//detect the continent of the selected country
+		var new_continent = $(this).find(':selected').parent().attr('class');
+		//detect the central part of the id (to consider inline fields as well)
+		var id_center = this.id.replace('id_','').replace('country','');
+		//select the detected continent in the corresponding object
+		$('#id_'+id_center+'continent [value="'+new_continent+'"]').attr('selected',true);
 	});
-	
+
+	//saves the complete countries list for restoring
+	var all_countries_html = $('#id_country').html();
+
+	//append the 'on' event to all the continent select boxes (live)
+	$('body').on('change', '[id^="id_"][id$="continent"]', function(){
+		//detect the selected continent
+		var new_continent = $(this).val();
+		//detect the central part of the id (to consider inline fields as well)
+		var id_center = this.id.replace('id_','').replace('continent','');
+		//restore the original contries list
+		$('#id_'+id_center+'country').html(all_countries_html);
+		//remove all the continent and countries other than the detected one
+		$('#id_'+id_center+'country').children().not('.'+new_continent).remove('optgroup');		
+	});
+
 });
+
